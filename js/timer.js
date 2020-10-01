023b001f15a7d55fdd80d9bb680d9741c4ab8ba3
alex89620109009 <alex89620109009@gmail.com>
2020-10-01 07:58:31 +0300

    Добавление видео

--- js/timer.js
+++ js/timer.js
@@ -9,7 +9,7 @@ var coutDownFunction = setInterval(function(){
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     if(hours <= 0 && days <= 0 && minutes <= 0 && seconds <= 0){
-
+        document.getElementById('open_video').style.display = "block";
         document.getElementById("time").innerHTML = "Мы открылись!"
         document.getElementById("time_h1").innerHTML = " "
     }
@@ -17,3 +17,6 @@ var coutDownFunction = setInterval(function(){
     document.getElementById("time").innerHTML = days + " дней " + hours + " часов " + minutes + " минут " + seconds + " секунд ";
     }
 })
+
+
+
